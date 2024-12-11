143dafee7a93b318743938fd47da23ba3950d933
regikov <nandrea835@gmail.com>
2024-12-11 09:32:54 +0100

    FIX: Reverting configuration settings

--- init-database.sh
+++ init-database.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
+# Connect to the default database 'postgres' and create the required databases
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    DROP DATABASE IF EXISTS news_app_db;
     CREATE DATABASE news_app_db;
+    CREATE DATABASE news_app_test;
 EOSQL
