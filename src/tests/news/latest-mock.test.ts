31c70ea6d447112a5a97dc907f0f00bb7d33383c
beta <beta.nycz@icloud.com>
2024-12-13 21:18:54 +0100

    updated tests arrangment

--- src/tests/news/latest-mock.test.ts
+++ src/tests/news/latest-mock.test.ts
@@ -9,19 +9,24 @@ jest.mock('../../utils.js');
 const mockFetchFromNewsAPI = fetchFromNewsAPI as jest.Mock;
 
 describe('news controller', () => {
+  let req: Request; 
+  let res: Response;
+
+  beforeEach(async () => {
+    req = {
+      query: { q: 'test', limit: '10', sortBy: 'popularity' },
+    } as unknown as Request;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as Response;
+  });
+
   it('should return articles with a 200 status code when fetchFromNewsAPI succeeds', async () => {
     const mockArticles = {
       articles: [{ title: 'News 1' }, { title: 'News 2' }],
     };
     mockFetchFromNewsAPI.mockResolvedValue(mockArticles);
-
-    const req = {
-      query: { q: 'test', limit: '10', sortBy: 'popularity' },
-    } as unknown as Request;
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
-    } as unknown as Response;
 
     await newsController.getLatestNews(req, res);
 
@@ -37,14 +42,6 @@ describe('news controller', () => {
   it('should return a 500 status code with an error message when fetchFromNewsAPI throws an error', async () => {
     const mockError = new Error('API Error');
     mockFetchFromNewsAPI.mockRejectedValue(mockError);
-
-    const req = {
-      query: { q: 'test', limit: '10', sortBy: 'popularity' },
-    } as unknown as Request;
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
-    } as unknown as Response;
 
     await newsController.getLatestNews(req, res);
 
@@ -93,7 +90,7 @@ describe('/latest', () => {
     });
   });
 
-  describe('query parameters', () => {
+  describe('valid query parameters', () => {
     it('should have correct limit parameter', async () => {
       await request(app).get('/api/news/latest?limit=11');
 
@@ -125,7 +122,7 @@ describe('/latest', () => {
     });
   });
 
-  describe('invalid parameters', () => {
+  describe('invalid query parameters', () => {
     describe('limit', () => {
       it('should fail with higher limit', async () => {
         const res = await request(app).get('/api/news/latest?limit=101');
